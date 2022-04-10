04a9da28ab08e82a6b3c13318a908c9ae3ddb713
satyanand <asto7>
2022-04-10 23:32:47 +0530

    add: basic results cards

--- client/src/components/Patient/Diagnosis/PatientResults.jsx
+++ client/src/components/Patient/Diagnosis/PatientResults.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 import { withUser } from "../../../context/user_context";
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const getTimestamp = (_id) => {
+  return new Date(parseInt(_id.slice(0, 8), 16) * 1000).toLocaleString();
+};
 
 function PatientResults(props) {
-  console.log(props);
-  return <div>PatientResults</div>;
+  const datas = props.userContext.user.patientResults || [];
+  const content = [];
+
+  for (let i = 0; i < datas.length; i++) {
+    const data = datas[i];
+    content.push(
+      <>
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              <span style={{ color: "grey" }}> Preliminary Output - </span>{" "}
+              {data.output
+                ? "More chance of heart attack"
+                : "Less chance of heart attack"}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Checked at - {getTimestamp(data._id)}
+            </Typography>
+          </CardContent>
+
+          <CardActions>
+            <Button>Get consultation by doctor</Button>
+          </CardActions>
+        </Card>
+        <br />
+      </>
+    );
+  }
+
+  return <Container>{content}</Container>;
 }
 
 export default withUser(PatientResults);
